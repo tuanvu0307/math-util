4a58713f90b5e7931f253468eec7734264e25d43
tuanvu0307 <nu251149082@gmail.com>
2020-11-14 12:16:14 +0700

    2nd: import static modification

--- src/vuldt/main/Test.java
+++ src/vuldt/main/Test.java
@@ -5,7 +5,8 @@
  */
 package vuldt.main;
 
-import vuldt.util.MathUtility;
+import static vuldt.util.MathUtility.*;
+// cau lenh khai bao san ham static khong can cham, y chang c, chi ap dung cho static
 
 /**
  *
@@ -15,16 +16,19 @@ public class Test {
 	public static void main(String[] args) {
 		//ta test ham cua ta bang mat 
 		long expected = 120; //ta mong nhan 120 neu goi ham 5!
-		long actualt = MathUtility.getFactorioal(5);
+		long actualt = getFactorioal(5);
 		System.out.println("5! Expected: "+expected+"; Actual: "+actualt);
 		
 		//expected: 720 if tinh 6!
-		System.out.println("6! Expected: "+expected+MathUtility.getFactorioal(6));
+		System.out.println("6! Expected: "+expected+getFactorioal(6));
 		
 		//expected: 1 if tinh 0!
-		System.out.println("0! Expected: "+expected+MathUtility.getFactorioal(0));
+		System.out.println("0! Expected: "+expected+getFactorioal(0));
 		
 		//expected: IllegalArgumentExcaption if tinh -5
-		System.out.println("-5! Expected: "+expected+MathUtility.getFactorioal(-5));
+		System.out.println("-5! Expected: "+expected+getFactorioal(-5));
+		
+		//in thu xem coi co giong Math.PI kh?
+		System.out.println("PI: "+PI);
 	}
 }
