23c268683f9b4d81bf9e119af5459e7b85ba4f87
tuanvu0307 <nu251149082@gmail.com>
2020-11-18 08:00:09 +0700

    optimize the getF() by using recursion

--- src/vuldt/util/MathUtility.java
+++ src/vuldt/util/MathUtility.java
@@ -22,20 +22,35 @@ public class MathUtility {
 	//21! cuc lon tran kieu long chua kh noi
 	// int toi da 2 ki tu 1 may
 	// hàm tính fgiai thuwad trong giới hán đầu vào từ 0..20
-	public static long getFactorioal(int n) {
-		if (n < 0 || n > 20) {
-			throw new IllegalArgumentException("n must be between 0..20");
-		}
-		if (n == 0 || n == 1) {
-			return 1;
-		}
-		//ko sai else, co else tru diem
-		long result = 1;
-		//doan nay thi n =2-20;
-		for (int i = 2; i <= n; i++) {
-			result *= i;
-		}
-		return result;
-	}
+	
+//	public static long getFactorioal(int n) {
+//		if (n < 0 || n > 20) {
+//			throw new IllegalArgumentException("n must be between 0..20");
+//		}
+//		if (n == 0 || n == 1) {
+//			return 1;
+//		}
+//		//ko sai else, co else tru diem
+//		long result = 1;
+//		//doan nay thi n =2-20;
+//		for (int i = 2; i <= n; i++) {
+//			result *= i;
+//		}
+//		return result;
+//	}
 
+	// Mình mimnh họa khái niệm refactoring - tối ưu code, chỉnh sửa lại code cho tốt hơn
+	// và mình minh họa lkhais niệm regression tét, tét hồi quy tét lại những gì ddaxx từng tét xanh để check xem có như
+	// ngày xưa không, sau khi đã sửa code 
+	
+	//viết đệ quy cho hoành traf́ng
+	public static long getFactorioal(int n) {
+		//gọi lại chính n=mình  với một quy mô khác 
+		//đệ  quy rất dễ bị lặp vô tận 
+		if(n<0 || n>20)
+			throw new IllegalArgumentException("n must be between 0..20");
+		if(n==0 || n==1)
+			return 1; //dieu kien dung
+		return n*getFactorioal(n-1);
+	}
 }
